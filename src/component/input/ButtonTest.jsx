c6fc2505d9bd9bd46cac3c053585158b8ebd75e2
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-02-12 14:40:49 +0330

    some small changes

--- src/component/input/ButtonTest.jsx
+++ src/component/input/ButtonTest.jsx
@@ -5,24 +5,22 @@ import React from 'react';
 const ButtonTest = () => {
     return (
         <>
-            <Box textAlign="center">
+            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 4 }}>
                 <h1>This is Button Component</h1>
-                <Stack direction="column" spacing={2} justifyContent="center" mb={3} sx={{bgcolor:"farshid.light"}}>
-                  <Box sx={{display:"flex", justifyContent:"center", gap:1}}>
+                <Stack direction="column" spacing={3} alignItems="center" mb={3} sx={{ bgcolor: "farshid.light", p: 4, borderRadius: 2 }}>
+                    <Box sx={{ display: "flex", gap: 2 }} >
                         <Button variant="text">Text</Button> {/* default variant is text */}
                         <Button variant="contained">Contained</Button>
                         <Button variant="outlined">Outlined</Button>
                         <Button href="#" variant="contained" color='secondary'>link contained Btn</Button>
-                  </Box>
-                  <Box>
-                    <Button variant='contained' color='warning' onClick={()=> alert("Clicked")}>controled Btn</Button>
-                  </Box>
+                    </Box>
+                    <Box>
+                        <Button variant='contained' color='warning' onClick={() => alert("Clicked")}>controled Btn</Button>
+                    </Box>
                 </Stack>
                 <Box>
                     <Button component={Link} to="/" variant='contained' color="success" >بازگشت به لیست</Button>
-
                 </Box>
-
             </Box>
 
         </>
