da760d7ca0a7bb681608b88001c3bd5077cf9c72
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-02-25 20:00:39 +0330

    Add Bootsrap-rtl.min to Project

--- src/component/2-input/TextFieldTest.jsx
+++ src/component/2-input/TextFieldTest.jsx
@@ -1,7 +1,25 @@
-import { Box, Button, Card, CardContent, TextField, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, MenuItem, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const TextFieldTest = () => {
+    const currencies = [
+        {
+            value: 'USD',
+            label: '$',
+        },
+        {
+            value: 'EUR',
+            label: '€',
+        },
+        {
+            value: 'BTC',
+            label: '฿',
+        },
+        {
+            value: 'JPY',
+            label: '¥',
+        },
+    ];
     return (
         <>
             <Box
@@ -21,7 +39,7 @@ const TextFieldTest = () => {
 
                 <Card sx={{ maxWidth: 500, }}>
 
-                    <CardContent sx={{display:"flex",flexWrap:"wrap", gap:2, justifyContent:"center"}}>
+                    <CardContent sx={{ display: "flex", flexWrap: "wrap", gap: 2, justifyContent: "center" }}>
                         <TextField
                             required
                             label="نام"
@@ -48,6 +66,54 @@ const TextFieldTest = () => {
 
                 </Card>
 
+            </Box>
+
+            <Typography variant="h4" color="initial" sx={{ direction: "rtl", fontWeight: "bold", textAlign: "right" }}>4-3: select</Typography>
+            <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+            <Box className="card">
+                <Box className="crad-body">
+                        <Box
+                            component="form"
+                            sx={{
+                                '& .MuiTextField-root': { m: 1, width: 300 },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <div>
+                                <TextField
+                                    select
+                                    label="Select"
+                                    defaultValue="EUR"
+                                    helperText="Please select your currency"
+                                >
+                                    {currencies.map((option) => (
+                                        <MenuItem key={option.value} value={option.value}>
+                                            {option.label}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                                <TextField
+                                    select
+                                    label="Native select"
+                                    defaultValue="EUR"
+                                    SelectProps={{
+                                        native: true,
+                                    }}
+                                    helperText="Please select your currency"
+                                >
+                                    {currencies.map((option) => (
+                                        <option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </TextField>
+                            </div>
+
+                        </Box>
+
+                </Box>
+            </Box>
             </Box>
 
 
