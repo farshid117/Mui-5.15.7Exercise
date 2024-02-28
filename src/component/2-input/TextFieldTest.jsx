e48a4c14bb66e6499adbd934a84aaa396220049a
alirez khayatpour <alireza_khayatpour@yahoo.com>
2024-02-28 20:15:07 +0330

    Add Adornment  TextField By 3 Approach

--- src/component/2-input/TextFieldTest.jsx
+++ src/component/2-input/TextFieldTest.jsx
@@ -1,7 +1,20 @@
-import { Box, Button, Card, CardContent, MenuItem, TextField, Typography } from "@mui/material";
+import {useState} from "react"
+import { Box, Button, Card, CardContent, MenuItem, TextField, Typography,
+    InputAdornment, FormControl, OutlinedInput, FormHelperText, InputLabel, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
 
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 const TextFieldTest = () => {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
     const currencies = [
         {
             value: 'USD',
@@ -76,12 +89,12 @@ const TextFieldTest = () => {
             <Box className="card p-4  w-75 m-auto my-4 align-items-center" >
                 <Box className="card-body" >
                     <Box
-                    
+
                         component="form"
                         sx={{
-                            direction:"rtl",
+                            direction: "rtl",
                             '& > :not(style)': { m: 1, width: 250 },
-                            
+
                         }}
                         noValidate
                         autoComplete="off"
@@ -154,6 +167,53 @@ const TextFieldTest = () => {
                     </Box>
                 </Box>
             </Box>
+
+            {/* //todo: TextFeild With Icon */}
+            <Typography variant="h4" color="initial" sx={{ direction: "rtl", fontWeight: "bold", textAlign: "right" }}>4-6: Icons</Typography>
+            <div className="card align-items-center w-75 m-auto my-4">
+                <div className="card-body">
+                    <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                        <div>
+                            <TextField
+                                label="With normal TextField"
+                                sx={{ m: 1, width: 200 }}
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">kg</InputAdornment>,
+                                }}
+                            />
+                            <FormControl sx={{ m: 1, width: '25ch' , }} >
+                                <InputLabel htmlFor="adornment-password">پسورد</InputLabel>
+                                <OutlinedInput
+                                    startAdornment={<InputAdornment position="start">آقا/خانم</InputAdornment>}
+                                    label=" &nbsp "
+
+                                />
+                                <FormHelperText >جنسیت</FormHelperText>
+                            </FormControl>
+
+                            <FormControl sx={{ m: 1, width: 200 }} variant="outlined">
+                                <InputLabel htmlFor="adornment-password">پسورد</InputLabel>
+                                <OutlinedInput
+                                    id="adornment-password"
+                                    type={showPassword ? 'text' : 'password'}
+                                    endAdornment={
+                                        <InputAdornment position="end">
+                                            <IconButton
+                                                onClick={handleClickShowPassword}
+                                                onMouseDown={handleMouseDownPassword}
+                                                edge="end"
+                                            >
+                                                {showPassword ? <VisibilityOff /> : <Visibility />}
+                                            </IconButton>
+                                        </InputAdornment>
+                                    }
+                                    label=" &nbsp "
+                                />
+                            </FormControl>
+                        </div>
+                    </Box>
+                </div>
+            </div>
 
 
             <Box sx={{ textAlign: "center", mt: 2 }}>
